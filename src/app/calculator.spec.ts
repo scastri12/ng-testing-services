6c168fec48d639cf4f89b84287be8b86766c8a0b
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-09-09 16:52:21 -0500

    ci: other matchers

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -9,4 +9,37 @@ describe('Test for Calculator', () => {
     //Assert
     expect(rta).toEqual(9);
   });
+  it('#multiply should return a four', () => {
+    //Arrange
+    const calculator = new Calculator();
+    //Act
+    const rta = calculator.multiply(4,1);
+    //Assert
+    expect(rta).toEqual(4);
+  });
+  it('#divide should return a some numbers', () => {
+    //Arrange
+    const calculator = new Calculator();
+
+    //Act & Assert
+    expect(calculator.divide(6,3)).toEqual(2);
+    expect(calculator.divide(5,2)).toEqual(2.5);
+  });
+  it('Test matchers', () => {
+   let name = 'nicolas';
+   let name2;
+
+   expect(name).toBeDefined();
+   expect(name2).toBeUndefined();
+
+   expect(1 + 3 === 4).toBeTruthy();
+   expect(1 + 1 === 3).toBeFalsy();
+
+   expect(5).toBeLessThan(10);
+   expect(20).toBeGreaterThan(10);
+
+   expect('123456').toMatch(/123/);
+   expect(['apples', 'oranges', 'pears']).toContain('oranges');
+
+  });
 });
